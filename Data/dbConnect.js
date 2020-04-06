e914b982c0c634b2cd5fecf29349a55741f801f7
Richard Ballard <36576579+Tricky-Ricky@users.noreply.github.com>
2020-04-06 09:30:58 +0100

    Database connection updates
    Added an error handler, which should help the DB re-connect under most circumstances.
    Added a reconnect function
    
    Should resolve issues where DB related errors would kill the bot process, and recreate connections when needed.

--- Data/dbConnect.js
+++ Data/dbConnect.js
@@ -1,36 +1,74 @@
 const mysql = require('mysql');
 
-let con = {
-  host: 'localhost',
-  port: '3306',
-  user: '******',
-  password: '*******',
-  database: '*******',
+let dbConfig = {
+  host: '*****', // Change to localhost for server
+  port: '*****', // Port for DB
+  user: '*****', // DB USER
+  password: '*****', // DB PASSWORD
+  database: '*****', // DB
 };
 
-let connection;
+//Create Connection
+let con = mysql.createConnection(dbConfig);
 
-function handleDisconnect() {
-  connection = mysql.createConnection(con);
-  connection.connect((err) => {
-    if(err) {
-      console.log('Error when connecting to DB:', err);
-      setTimeout(handleDisconnect, 2000);
-    } else {
-      console.log('Connected to DB!');
-    }
+// Establish New Connection
+con.connect((err) => {
+  if (err) {
+    console.log('Error Connecting: ' + err.stack);
+    con = reconnect(con);
+  }else {
+    console.log('Connected To DB');
+  }
+});
+
+// Reconnect
+function reconnect(con){
+  console.log("Reconnecting...");
+  // Destroy current connection
+  if(con){
+    con.destroy();
+  }
+  // Recreate
+  con = mysql.createConnection(dbConfig);
+  // Attempt to connect
+  con.connect((err) => {
+      if(err) {
+        // Attempt every 5 seconds
+        setTimeout(reconnect, 5000);
+      }else {
+        console.log('New Connection made to DB');
+        return con;
+      }
   });
-
-  connection.on('error', (err) => {
-    console.log('Database error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
-      handleDisconnect();
-    }else {
-      throw err;
-    }
-  })
 }
 
-handleDisconnect();
+// Error Listener
+con.on('error', (err) => {
+  // Server closes the connection
+  if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+    console.log('Cannot establish connection with DB: ' + err.code);
+    return reconnect(con);
+  }
+  // Connection in closing 
+  else if(err.code === 'PROTOCOL_ENQUEUE_AFTER_QUIT'){
+    console.log('Cannot establish connection with DB: ' + err.code);
+    return reconnect(con);
+  }
+  // Fatal Error: Connection must be recreated
+  else if(err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR'){
+    console.log('Cannot establish connection with DB: ' + err.code);
+    return reconnect(con);
+  }
+  // Connection already being established
+  else if(err.code === 'PROTOCOL_ENQUEUE_HANDSHAKE_TWICE'){
+    console.log('Cannot establish connection with DB: ' + err.code);
+  }
+  // Any other error
+  else {
+    console.log('Cannot establish connection with DB: ' + err.code);
+    return reconnect(con);
+  }
+})
 
-module.exports = connection;
+// Export 
+module.exports = con;
